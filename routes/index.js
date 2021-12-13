2d9d4bda68a07b2fdd8eb0bdabcca1207e0ad742
marioe997 <marioe997@gmail.com>
2021-12-13 10:28:32 -0600

    Prueba

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  dogstatsd.increment('node.page.views', ['method.GET', 'route.contacs'])
   res.send('<p>Hola mario!!</p>');
 });
 
