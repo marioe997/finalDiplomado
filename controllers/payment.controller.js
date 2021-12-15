b554962adfdb9a2cc209a44d3de8858dc4d7d39f
marioe997 <marioe997@gmail.com>
2021-12-14 19:34:31 -0600

    payment add

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -2,9 +2,12 @@ const PAYMENT_FILE_PATH = '../payment-generated.txt';
 const faker = require('faker');
 const fs = require('fs');
 const LINE_ENDING = require('os').EOL;
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
 
 module.exports = {
     create: function (req, res) {
+        dogstatsd.increment('Payment.create');
         const fd = fs.openSync(PAYMENT_FILE_PATH, 'a');
         fs.appendFileSync(fd, faker.commerce.price() + LINE_ENDING, 'utf8');
         res.json({ message: "Se creo exitosamente un registro"});
@@ -12,6 +15,7 @@ module.exports = {
     },
 
     applyDiscount: function (req, res) {
+    dogstatsd.increment('Payment.applyDiscount');
     //debera de restar una cantidad a cada precio en payment-generated.txt
     let price = faker.commerce.price();
     console.log(price)
@@ -28,6 +32,7 @@ module.exports = {
     },
 
     getPromos: function (req, res) {
+    dogstatsd.increment('Payment.getPromos');
         res.json([  
             {name: "BUENFIN"},
             {name: "HOTSALE"},
