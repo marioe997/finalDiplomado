b3fca1eb2c8b881e0ab599d6f9669f07376fd690
marioe997 <marioe997@gmail.com>
2021-12-14 20:01:11 -0600

    se agrega dogstatsd

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,13 +1,19 @@
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
+
 module.exports = {
     create: function (req, res) {
+        dogstatsd.increment('task.create');
         res.json([{id: 1, name: 'QA Test App'}]);
     },
 
     getAll: function (req, res) {
+        dogstatsd.increment('task.getAll');
         res.json([{id: 1, name: 'QA Test App'}]);
     },
 
     getById: function (req, res) {
+        dogstatsd.increment('task.getById');
         res.json({id: 1, name: 'QA Test App'});
     }
 };
