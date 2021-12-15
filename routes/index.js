a555e11c7c227c162afdbbafa2570974465203be
marioe997 <marioe997@gmail.com>
2021-12-15 12:09:06 -0600

    fix: se corrige index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-const index = path.resolve('./main.html');
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 var StatsD = require('hot-shots');
 var dogstatsd = new StatsD();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   dogstatsd.increment('my_counter');
-  res.send(index);
+  res.sendFile(path.join(__dirname, '../main.html'));
 });
 
 module.exports = router;
