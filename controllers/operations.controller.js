33f9c359e0e390e427cfdddca020c6cb69fdfff9
marioe997 <marioe997@gmail.com>
2021-12-14 19:18:08 -0600

    Se agrega dogstatssd

--- controllers/operations.controller.js
+++ controllers/operations.controller.js
@@ -1,23 +1,30 @@
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
+
 module.exports = {
     sum: function (req, res) {
+        dogstatsd.increment('operations.sum');
         let { number1, number2 } = req.params;
         res.json({name: 'Suma', suma: parseInt(number1) + parseInt(number2)});
         res.status(201).send();
     },
 
     substract: function (req, res) {
+        dogstatsd.increment('operations.substract');
         let { number1, number2 } = req.params;
         res.json({name: 'Resta', Resta: parseInt(number1) - parseInt(number2)});
         res.status(201).send();
     },
 
     multiply: function (req, res) {
+        dogstatsd.increment('operations.multyply');
         let { number1, number2 } = req.params;
         res.json({name: 'Multiplicacion', Multiplicacion: parseInt(number1) * parseInt(number2)});
         res.status(201).send();
     },
 
     divide: function (req, res) {
+        dogstatsd.increment('operations.divide');
         let { number1, number2 } = req.params;
         if(parseInt(number2) === 0 ){
             res.json({name: 'Division', Division: "No se puede dividir entre 0 intenta otro numero"});
